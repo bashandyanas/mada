6ccc7f4b0356e98a348e6aa16543e673def0471e
bashandyanas <bashandyanas@gmail.com>
2015-05-25 12:08:57 +0200

    another fix

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-<<<<<<< HEAD
   $('#logo').click(function() {
     $('#myCarousel').toggle();
     $('body').css('background-color', 'rgb(200,100,255)', 'slow');
@@ -7,13 +6,3 @@ $(document).ready(function() {
     $('.menu').toggleClass('hidden').animate();
   })
 });
-=======
-  $('.menuhover').mouseenter(function() {
-    $('.itemactive').blur();
-  })
-});
-
-$( "#logo" ).click(function() {
-  $( ".itemactive" ).blur();
-});
->>>>>>> a02528fbf2e10126244ccdccb195bbc306249760
